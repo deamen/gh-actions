8321ae0dadf28c65f7b1e3f42220b8a91d627581
Song Tang <stang@mmz.au>
2024-07-21 19:23:34 +1000

    Trust the GitHub Actions workspace directory

--- check-binary-files/entrypoint.sh
+++ check-binary-files/entrypoint.sh
@@ -1,22 +1,28 @@
 #!/bin/sh -l
 
+# Trust the GitHub Actions workspace directory
+git config --global --add safe.directory /github/workspace
+
 # Initialize an empty variable to store binary file names
 binary_files=""
 
 # Get the list of commits in the push
 commits=$(git rev-list "$2" ^"$1")
-echo "Commit:" $commits
 
 # Check each commit for binary files
 for commit in $commits; do
   files=$(git diff-tree --no-commit-id --name-only --diff-filter=AM -r $commit)
-  echo "files: $files"
   
-  for file in $files; do
-    if file "$file" | grep -q 'ELF\|PE32\|Mach-O'; then
-      binary_files="$binary_files$file (in commit $commit)\n"
-    fi
-  done
+  if [ -n "$files" ]; then
+    for file in $files; do
+      if file "$file" | grep -q 'ELF\|PE32\|Mach-O'; then
+        binary_files="$binary_files$file (in commit $commit)\n"
+      fi
+    done
+  else
+    echo "No files detected in commit $commit"
+    exit 1
+  fi
 done
 
 # Set the output variable if binary files were detected
@@ -28,4 +34,3 @@ if [ -n "$binary_files" ]; then
 else
   echo "No binary files detected."
 fi
-
