61d2ee7093deea7d19adaa54ad01b018fe5b2b35
deamen <deamen@gmail.com>
2025-03-03 17:58:55 +1100

    Add proper support for rhel9 in setup-buildah/install-buildah.sh
    Fixes #16
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/deamen/gh-actions/issues/16?shareId=XXXX-XXXX-XXXX-XXXX).

--- setup-buildah/install-buildah.sh
+++ setup-buildah/install-buildah.sh
@@ -11,13 +11,23 @@ install_ubuntu_fedora() {
 
 install_rhel() {
   sudo yum install -y buildah
+  install_qemu_static_rhel9
+}
+
+install_qemu_static_rhel9() {
+  wget -c https://github.com/multiarch/qemu-user-static/releases/download/v7.2.0-1/qemu-aarch64-static -P /tmp/
+  chmod a+x /tmp/qemu-aarch64-static
+  sudo cp /tmp/qemu-aarch64-static /usr/bin/
+  wget -c https://raw.githubusercontent.com/qemu/qemu/e75941331e4cdc05878119e08635ace437aae721/scripts/qemu-binfmt-conf.sh -P /tmp/
+  chmod a+x /tmp/qemu-binfmt-conf.sh
+  sudo /tmp/qemu-binfmt-conf.sh --qemu-suffix "-static" --qemu-path /usr/bin/
 }
 
 case "$OS" in
   ubuntu|fedora)
     install_ubuntu_fedora
     ;;
-  rhel)
+  rhel|rhel9)
     install_rhel
     ;;
   *)
