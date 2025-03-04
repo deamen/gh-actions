8db1f7730cd222ef74503d4f524dd0e34275ab0c
Song Tang <stang@mmz.au>
2025-03-04 12:08:31 +1100

    Properly set up qemu-static in RHEL

--- setup-buildah/install-buildah.sh
+++ setup-buildah/install-buildah.sh
@@ -3,6 +3,10 @@
 set -e
 
 OS=$1
+QEMU_STATIC_VER="v7.2.0-1"
+QEMU_VER="v9.2.2"
+
+export QEMU_TARGET_LIST="aarch64"
 
 install_ubuntu_fedora() {
   sudo apt-get update
@@ -15,12 +19,18 @@ install_rhel() {
 }
 
 install_qemu_static_rhel() {
-  wget -c https://github.com/multiarch/qemu-user-static/releases/download/v7.2.0-1/qemu-aarch64-static -P /tmp/
-  chmod a+x /tmp/qemu-aarch64-static
+  wget -c https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_STATIC_VER}/qemu-aarch64-static -P /tmp/
+  chmod 0755 /tmp/qemu-aarch64-static
   sudo cp /tmp/qemu-aarch64-static /usr/bin/
-  wget -c https://raw.githubusercontent.com/qemu/qemu/e75941331e4cdc05878119e08635ace437aae721/scripts/qemu-binfmt-conf.sh -P /tmp/
+
+  wget -c https://raw.githubusercontent.com/qemu/qemu/refs/tags/${QEMU_VER}/scripts/qemu-binfmt-conf.sh -P /tmp/
   chmod a+x /tmp/qemu-binfmt-conf.sh
-  sudo /tmp/qemu-binfmt-conf.sh --qemu-suffix "-static" --qemu-path /usr/bin/
+  # Patch qemu-binfmt-conf.sh so it picks up QEMU_TARGET_LIST
+  patch /tmp/qemu-binfmt-conf.sh qemu-binfmt-conf-aarch.patch
+  sudo -E  /tmp/qemu-binfmt-conf.sh --qemu-suffix "-static" --qemu-path /usr/bin --persistent yes --preserve-argv0 yes
+
+  # To reset the binfmt_misc entries:
+  # sudo find /proc/sys/fs/binfmt_misc -type f -name 'qemu-*' -exec sh -c 'echo -1 > {}' \;
 }
 
 case "$OS" in
